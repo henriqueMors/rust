750abfbe88e98208f2cb12941983d81b1f8f96cb
Henrique Moraes <ahmdesouza@gmail.com>
2024-12-17 23:05:04 -0300

    update

---
+++ Fundamentos/target/debug/Fundamentos.d
@@ -0,0 +1,1 @@
+/home/mors/Documents/GitHub/rust/Fundamentos/target/debug/Fundamentos: /home/mors/Documents/GitHub/rust/Fundamentos/src/main.rs
---
+++ Fundamentos/target/debug/deps/Fundamentos-de13a81edced1be4.d
@@ -0,0 +1,5 @@
+/home/mors/Documents/GitHub/rust/Fundamentos/target/debug/deps/Fundamentos-de13a81edced1be4: src/main.rs
+
+/home/mors/Documents/GitHub/rust/Fundamentos/target/debug/deps/Fundamentos-de13a81edced1be4.d: src/main.rs
+
+src/main.rs:
