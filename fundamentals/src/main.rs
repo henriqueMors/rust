08ba36d6f9c2b3fda16e1eecaf2b1e8c873088d5
Henrique Moraes <ahmdesouza@gmail.com>
2024-12-19 18:03:50 -0300

    Update main.rs

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
-    println!("Hello, world! Versão Cargo!");
-    println!("Que venha o rust!")
+    //println!("Hello, world! Versão Cargo!");
+    //println!("Que venha o rust!")
+
+    println!("Inicio do programa");
+    let mut x = 5;
+    println!("O valor de x é: {x}");
+
+    x = 6;
+
+    let x = 666;
+    println!("O valor de x agora é: {x}");
+
+    let mut y = 5;
+    println!("O valor de y é: {y}");
+    y = 6;
+    println!("O valor de y agora é: {y}")
 }
