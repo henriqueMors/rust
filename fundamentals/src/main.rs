340cec70d6899ffb17d3955b14590c9e19a2cacc
Henrique Moraes <ahmdesouza@gmail.com>
2024-12-20 15:44:06 -0300

    Update main.rs

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -2,14 +2,14 @@ fn main() {
 
     //boas vindas ao rust c
 
-    //println!("Hello, world! Versão Cargo!");
-    //println!("Que venha o rust!")
+    //println!("Hello, world!!");
+    //println!("Bem vindo ao RUST!")
 
     
     //variaveis e mutabilidade
     
     /*
-    println!("Inicio do programa");
+    println!("Inicio amostra de mutabilidade");
     let mut x = 50;        //o i32 vem automaticamente pelo codigo
                                 //let é uma variavel imutavel. mutavel com MUT
     println!("O valor de x é: {x}"); //abre e fecha chaves e insere a variavel
@@ -25,15 +25,57 @@ fn main() {
     println!("O valor de y agora é: {y}") 
     */
 
+    /*
     const UMA_HORA_EM_SEGUNDOS: i32 = 1 * 60 * 60;
 
-    println!("Iniciando...");
+    println!("Iniciando mutabilidade...");
     let mut x = 1;
     println!("Até aqui, x equivale a {x}");
 
-    println!("A partir de agora, mudará de valor para \nmostrar quantos segundos existem em uma hora");
+    println!("A partir de agora, mudará \nde valor para mostrar quantos \nsegundos existem em uma hora.");
     x = UMA_HORA_EM_SEGUNDOS;
     println!("Então, x agora vale {x} segundos");
-    println!("...Encerrando.")
- 
+    println!("...Encerrando.");
+    */
+
+    //blocos e sombreamentos
+    /*
+    println!("Iniciando blocos...");
+    const X: i32 = 5;
+    let y = 6;
+    let mut z = 7;
+    z = z + 1;
+
+    println!("fora do bloco, os numeros sao: x={X}, y={y} e z={z}.");
+
+    {
+        const X: i32 = 555;
+        let y = 666;
+        let mut z = 777;
+        z = z + 1;
+        println!("Dentro do bloco, os numeros sao: x={X}, y={y} e z={z}.");
+    }
+
+    println!("fora novamente do bloco, os numeros voltam a ser: x={X}, y={y} e z={z}.");
+
+    println!("Encerrando...");
+    */
+
+
+    println!("Iniciando sombreamento...");
+    let x = 5;
+    println!("O valor de x é {x}.");
+    let x = x + 1;
+    println!("O valor de x é {x}.");
+
+    {
+        let x = x * 2; //ele acessa o ultimo x (variavel) com esse valor fora do bloco
+        println!("O valor de x no bloco interno é {x}.");
+    }
+    println!("fora do bloco novamente... x={x}.");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+
 }
