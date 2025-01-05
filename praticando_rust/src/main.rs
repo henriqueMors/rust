7f234ad20d21e7fd1f9eb307e9284a867151e017
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-04 21:49:59 -0300

    update na pratica

--- praticando_rust/src/main.rs
+++ praticando_rust/src/main.rs
@@ -24,6 +24,46 @@ fn main() {
 
     */
 
-
+    use std::io;
+    
+    println!("Bóra calcular números primos!");
+    
+        println!("Digite o número para saber se ele é primo: ");
+        let mut numero = String::new();
+    
+        io::stdin()                             //ler entrada de dados pelo teclado
+            .read_line(&mut numero)             //carrega consigo uma quebra de linha* usei o .trim() para retirar a quebra de linha
+            .expect("Falha ao ler a entrada");
+    
+        let numero: u64 = match numero.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Por favor, insira um número válido.");
+                return;
+            }
+        };
+    
+        if is_primo(numero) {
+            println!("O número {numero} é primo!");
+        } else {
+            println!("O número {numero} não é primo!");
+        }
+    
+    // Função para verificar se um número é primo
+    fn is_primo(numero: u64) -> bool {
+        if numero <= 1 {
+            return false;
+        }
+    
+        let mut divisor: u64 = 2;
+            while divisor * divisor <= numero {
+                if numero % divisor == 0 {
+                    return false;
+            }
+            divisor += 1;
+        }
+    
+        true
+    }
 
 }
