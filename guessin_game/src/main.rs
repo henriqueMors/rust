712fa8eaf4257941cea48231f0f9eb21e552d462
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-07 20:25:57 -0300

    update

--- guessin_game/src/main.rs
+++ guessin_game/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Advinhe o número!");
+
+    println!("Insira o número que acha ser...");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falha na leitura");
+
+    let inserted = guess.trim();
+
+    println!("Você digitou: {}.", inserted);
+
 }
