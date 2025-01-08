08e22da84bd7dea864735e9561845abe816c4df1
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-07 22:22:46 -0300

    Update main.rs

--- guessin_game/src/main.rs
+++ guessin_game/src/main.rs
@@ -1,25 +1,43 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 
 fn main() {
-/* 
+
     println!("Advinhe o número!");
 
-    println!("Insira o número que acha ser...");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
 
-    io::stdin()
+        //println!("O número secreto é: {secret_number}");
+
+        let mut guess = String::new();
+
+        io::stdin()
         .read_line(&mut guess)
         .expect("Falha na leitura");
 
-    let inserted = guess.trim();
 
-    println!("Você digitou: {}.", inserted);
+        let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+        //let inserted = guess.trim();
+        };
 
-     */
+        println!("Você digitou: {guess}.");
 
-    let x = 5;
-    let y = 3;
-    println!("x = {x} e {y} + 2 = {}", y + 2);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("é baixo..."),
+            Ordering::Greater => println!("é alto..."),
+            Ordering::Equal => {
+                println!("Acertou!!!");
+                break;
+            } 
+        }
+
+    }
+
 
 }
