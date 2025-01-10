fe0bd09d896aafa324f87666427aa938ba577a72
Henrique Moraes <92050086+henriqueMors@users.noreply.github.com>
2025-01-09 22:42:37 -0300

    Update main.rs

--- kick_space/src/main.rs
+++ kick_space/src/main.rs
@@ -10,29 +10,37 @@ fn main() {
         io::stdin()
             .read_line(&mut challenging)
             .expect("Falha ao ler a entrada");
-        println!("Você digitou o desafio: {},\nagora, é a vez do próximo acertar essa quantidade, \nsomente segurando a barra de espaço... \nboa sorte!", challenging.trim());
-        println!("#Segure a barra de espaço e pressione Enter quando terminar#");
-    
     let challenging: usize = match challenging.trim().parse() { // alterando o tipo da variavel para que seja lido como numero
             Ok(num) => num, // vai tratar o erro de caso o usuario digite algo fora do necessario
             Err(_) => {
                 println!("Por favor, insira um número válido!");
-                return; // Sai do programa
+                return; // sai do programa #preciso entender melhor o uso do 'return;'#
             }
         };
+    
+        println!("Você digitou o desafio: {},\nagora, é a vez do próximo acertar essa quantidade, \nsomente segurando a barra de espaço.", challenging.trim());
 
-    let mut key_space = String::new(); // armazena a quantidade de espacos
+    let mut key_space = String::new();
+    loop {
+        println!("#Pressione somente a barra de espaço e pressione Enter quando terminar...# \nGood LucK!");
+        key_space.clear(); // limpa a entrada anterior
         io::stdin()
             .read_line(&mut key_space)
             .expect("Falha ao ler a entrada");
+        
+        if key_space.trim().chars().all(|c| c == ' ') { // verifica se todos os caracteres sao espacos
+            break; // se todos forem espacos, sai do loop
+        } else {
+            println!("Entrada inválida! Digite apenas espaços.");
+        }
+    }
 
     let space_count = key_space.chars().filter(|&c| c == ' ').count();
-    
-    /*  
-    space_count recebe a quantidade de espacos digitados.
-    key_space esta configurado para receber e aceitar 
-    o tipo de caracter espaco, inserido no teclado.
-    c é uma configuracao de caracter
+    /*
+            space_count recebe a quantidade de espacos digitados.
+            key_space esta configurado para receber e aceitar 
+            o tipo de caracter espaco, inserido no teclado.
+            c é uma configuracao de caracter (mais ou menos o que o loop faz)
     */
 
     if space_count == challenging { // um IF/ELSE somente para um comparativo
