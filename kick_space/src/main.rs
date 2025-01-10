aeb825cd0ff50cf3ea85bc3b843a33eba12f3bd4
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-09 22:21:25 -0300

    Update main.rs

--- kick_space/src/main.rs
+++ kick_space/src/main.rs
@@ -11,8 +11,15 @@ fn main() {
             .read_line(&mut challenging)
             .expect("Falha ao ler a entrada");
         println!("Você digitou o desafio: {},\nagora, é a vez do próximo acertar essa quantidade, \nsomente segurando a barra de espaço... \nboa sorte!", challenging.trim());
-        
-    let challenging: usize = challenging.trim().parse().expect("Por favor, insira um número válido!"); // alterando o tipo da variavel para que seja lido como numero
+        println!("#Segure a barra de espaço e pressione Enter quando terminar#");
+    
+    let challenging: usize = match challenging.trim().parse() { // alterando o tipo da variavel para que seja lido como numero
+            Ok(num) => num, // vai tratar o erro de caso o usuario digite algo fora do necessario
+            Err(_) => {
+                println!("Por favor, insira um número válido!");
+                return; // Sai do programa
+            }
+        };
 
     let mut key_space = String::new(); // armazena a quantidade de espacos
         io::stdin()
@@ -20,6 +27,7 @@ fn main() {
             .expect("Falha ao ler a entrada");
 
     let space_count = key_space.chars().filter(|&c| c == ' ').count();
+    
     /*  
     space_count recebe a quantidade de espacos digitados.
     key_space esta configurado para receber e aceitar 
@@ -27,7 +35,7 @@ fn main() {
     c é uma configuracao de caracter
     */
 
-    if space_count == challenging { // um IF/ELSE somente para um comparativo simples
+    if space_count == challenging { // um IF/ELSE somente para um comparativo
         println!("VOCÊ ACERTOU!!!");
     } else {
         println!("Errooooooooooouu, a quantidade era de {challenging}!");
