ad69e43ec293324f38477fe45f5b8b59886e0e9f
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-12 02:05:11 -0300

    update
    updade do readme e inicio da criacao do modo de jogo ocm numero randomico

--- kick_space/src/main.rs
+++ kick_space/src/main.rs
@@ -26,12 +26,16 @@ fn keywar_title() {
     println!("@#=-KEY WAR-=#@");
 }
 
+fn instruction_01() {
+    println!("#Pressione somente a barra de espaço e pressione Enter quando terminar# \nGood LucK!");
+}
 
 
 
 
-// (uso futuro) gerador automático
-fn _random_number() -> i32 { // -> usado para que ele seja um return com um valor i32
+
+// gerador automático
+fn random_number() -> usize { // -> usado para que ele seja um return com um valor i32
     let mut rng = rand::thread_rng(); // gerador de numeros aleatorios
     rng.gen_range(1..=100) // retorna um número entre 1 e 100
 }
@@ -64,6 +68,49 @@ fn continue_game() -> bool {
 
 
 
+fn keywar_game_random_number() {
+    
+    println!("Como está sua percepção?");
+
+    let mut challenging = random_number(); // o desafiante digita quantidade de a ser acertada pelo desafiado
+    
+        println!("Hora de testar! Seu desafio: {}!\n.", challenging);
+
+    let mut key_space = String::new();
+    loop {
+        instruction_01();
+        key_space.clear(); // limpa a entrada anterior
+        io::stdin()
+            .read_line(&mut key_space)
+            .expect("Falha ao ler a entrada");
+        
+        if key_space.trim().chars().all(|c| c == ' ') { // verifica se todos os caracteres sao espacos
+            break; // se todos forem espacos, sai do loop
+        } else {
+            println!("Entrada inválida! Digite apenas espaços.");
+        }
+    }
+
+    let space_count = key_space.chars().filter(|&c| c == ' ').count();
+    /*
+            space_count recebe a quantidade de espacos digitados.
+            key_space esta configurado para receber e aceitar 
+            o tipo de caracter espaco, inserido no teclado.
+            c é uma configuracao de caracter (mais ou menos o que o loop faz)
+    */
+
+    if space_count == challenging { // um IF/ELSE somente para um comparativo
+        println!("VOCÊ ACERTOU!!!");
+    } else {
+        println!("Errooooooooooouu, a quantidade era de {challenging}!");
+    }
+
+    println!("Você digitou {space_count} caracteres de espaço."); // mensagem de encerramento
+}
+
+
+
+
 
 fn keywar_game_challenging() {
     
@@ -86,7 +133,7 @@ fn keywar_game_challenging() {
 
     let mut key_space = String::new();
     loop {
-        println!("#Pressione somente a barra de espaço e pressione Enter quando terminar# \nGood LucK!");
+        instruction_01();
         key_space.clear(); // limpa a entrada anterior
         io::stdin()
             .read_line(&mut key_space)
@@ -120,6 +167,6 @@ fn keywar_game_challenging() {
 
 
 // selecionar o modo do game
-fn game_mode() {
+fn _game_mode() {
 
 }
