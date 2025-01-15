1c8734b6d9edcc08d4b4f60959a9c9f02b00168f
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-14 23:21:56 -0300

    Update keywar_game_random_number.rs

--- kick_space/src/game/keywar_game_random_number.rs
+++ kick_space/src/game/keywar_game_random_number.rs
@@ -4,15 +4,15 @@ use crate::game::random_number::random_number;
 use crate::game::instruction::instruction_01;
 use crate::game::check_result::check_result;
 
-pub fn keywar_game_random_number() {
+pub fn keywar_game_random_number() { //modo de jogo com numero randomico
     println!("Como está sua percepção?");
-    let challenging = random_number();
+    let challenging = random_number(); //gera um numero randomico e armazena em challenging ... entre 1 e 100 (pode-se aumentar)
 
     println!("Hora de testar! Seu desafio: {}!", challenging);
 
-    let mut key_space = String::new();
+    let mut key_space = String::new(); //faz o armazenamento dos espacos digitados
     loop {
-        instruction_01();
+        instruction_01(); //instrucao de jogo
         key_space.clear();
         io::stdin()
             .read_line(&mut key_space)
