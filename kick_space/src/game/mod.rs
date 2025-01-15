75d9581ed74c7873319f5375a0a8b640b7d0b6d0
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-14 23:27:25 -0300

    Update mod.rs

--- kick_space/src/game/mod.rs
+++ kick_space/src/game/mod.rs
@@ -1,3 +1,7 @@
+//usado para organizar e gerenciar os submodulos de uma pasta no Rust. Ele funciona como o ponto central de configuracao de modulos, permitindo que outros arquivos (como main.rs) acessem os submodulos declarados na pasta.
+//pub é para deixar a funcao publica
+
+
 pub mod random_number;
 pub mod game_mode;
 pub mod keywar_game_random_number;
