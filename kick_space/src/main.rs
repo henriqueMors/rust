8bac65a5376d4b6e597abc6971eda08e29005cca
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-18 15:57:28 -0300

    update

--- kick_space/src/main.rs
+++ kick_space/src/main.rs
@@ -1,11 +1,12 @@
 //main é o arquivo principal, aqui executa-se todo o software
 mod game;
+use colored::*;
 
 use game::game_mode::game_mode;
 use game::continue_game::continue_game;
 
 fn main() {
-    println!("@#=-KEY WAR-=#@");
+    println!("@#=-KEY WAR-=#@".green().bold());
 
     loop {
         game_mode();
