1cf730c139601f58dd637f9bb46ebf019656349d
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-25 00:20:10 -0300

    Update create_player.rs

--- kick_space/src/game/create_player.rs
+++ kick_space/src/game/create_player.rs
@@ -2,13 +2,13 @@ use diesel::prelude::*;
 use crate::schema::bestplayers;
 
 #[derive(Insertable)]
-#[table_name = "bestplayers"]
+#[diesel(table_name = bestplayers)] // Atualize se o nome da tabela for diferente
 pub struct NewPlayer<'a> {
     pub name: &'a str,
     pub quantity: i32,
 }
 
-pub fn create_player(conn: &SqliteConnection, name: &str) {
+pub fn create_player(conn: &mut SqliteConnection, name: &str) { // Conexão mutável
     let new_player = NewPlayer { name, quantity: 0 };
 
     diesel::insert_into(bestplayers::table)
