c31ac9f49c37f3aaeb90389109e0869d64a5b78d
Henrique Moraes <ahmdesouza@gmail.com>
2025-01-29 23:54:49 -0300

    Update instruction.rs

--- kick_space/src/game/instruction.rs
+++ kick_space/src/game/instruction.rs
@@ -4,6 +4,6 @@ pub fn instruction_01() {
     println!("#Pressione somente a barra de espaço e pressione Enter quando terminar# \nGood LucK!");
 }
 
-pub fn instruction_02() {
+pub fn _instruction_02() {
     println!("#Modo Desafiado: Tente acertar o número exato de espaços!# \nBoa sorte!");
 }
