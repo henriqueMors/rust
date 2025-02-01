51ad2a85b728cd4616dbcfa6d0629052f5bee99f
Henrique Moraes <ahmdesouza@gmail.com>
2025-02-01 13:58:48 -0300

    update
    best player screen

--- kick_space/src/game/check_result.rs
+++ kick_space/src/game/check_result.rs
@@ -1,7 +1,8 @@
 //checagem de selecao de mensagem conforme resultado
 use rand::Rng; //biblioteca para obter um numero randomico
+use crate::db::{establish_connection, save_player};
 
-pub fn check_result(space_count: usize, challenging: usize) {
+pub fn check_result(space_count: usize, challenging: usize, player_name: &str) {
     if space_count == challenging {
         let messages = vec![
             "VOCÊ ACERTOU!!!",
@@ -13,8 +14,12 @@ pub fn check_result(space_count: usize, challenging: usize) {
         let mut rng = rand::thread_rng();
         let random_index = rng.gen_range(0..messages.len());
         println!("{}", messages[random_index]);
-    } else if (space_count as isize - challenging as isize).abs() <= 6 {
-        println!("Quase lá! O desafio era {}. Tente novamente!", challenging);
+
+        // Salva o jogador no banco de dados
+        let mut conn = establish_connection();
+        if let Err(e) = save_player(&mut conn, player_name, space_count as i32) {
+            eprintln!("Erro ao salvar o jogador no banco de dados: {}", e);
+        }
     } else {
         println!("Errooooooooooouu, a quantidade era de {}!", challenging);
     }
